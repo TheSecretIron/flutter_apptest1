124603dabf6aee00df8800e3adcc4dba7ef96a60
TheSecretIron <70634067+TheSecretIron@users.noreply.github.com>
2020-09-12 15:38:37 +0800

    The Big Update 1.2

--- lib/info.dart
+++ lib/info.dart
@@ -5,14 +5,100 @@ class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton.icon(
-          color: Colors.red,
-          icon: Icon(Icons.add_a_photo), //`Icon` to display
-          label: Text('Add Photo'), //`Text` to display
-          onPressed: () {},
-        ),
-      ),
+      body: Stack(
+        children: <Widget> [
+          Align(
+              alignment: AlignmentDirectional.topStart,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                "S.O.P",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                  "Alat Perlindungan",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+          ),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                  "Fun Facts",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+          ),
+          Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                  "Berita Palsu",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+          ),
+          Align(
+              alignment: Alignment.bottomLeft,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                  "More Info",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+          ),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: FlatButton(
+                color: Colors.purple[400],
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {},
+                child: Text(
+                  "Reminder",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              )
+          ),
+        ],
+      )
     );
   }
 }
