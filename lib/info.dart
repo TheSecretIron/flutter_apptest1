a835c240b744f8754eba04339beba086c6014123
TheSecretIron <70634067+TheSecretIron@users.noreply.github.com>
2020-09-13 11:03:06 +0800

    The Big Update 1.2

--- lib/info.dart
+++ lib/info.dart
@@ -5,99 +5,122 @@ class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget> [
-          Align(
-              alignment: AlignmentDirectional.topStart,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                "S.O.P",
-                style: TextStyle(fontSize: 20.0),
-              ),
-            )
-          ),
-          Align(
-              alignment: Alignment.topRight,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                  "Alat Perlindungan",
-                  style: TextStyle(fontSize: 20.0),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                  color: Colors.purple[400],
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/1.jpg',
+                        height: 140.0,
+                        width: 140.0,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-          ),
-          Align(
-              alignment: Alignment.centerLeft,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                  "Fun Facts",
-                  style: TextStyle(fontSize: 20.0),
+                RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                  color: Colors.purple[400],
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/2.jpg',
+                        height: 140.0,
+                        width: 140.0,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-          ),
-          Align(
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                  "Berita Palsu",
-                  style: TextStyle(fontSize: 20.0),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                  color: Colors.purple[400],
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/3.jpg',
+                        height: 140.0,
+                        width: 140.0,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-          ),
-          Align(
-              alignment: Alignment.bottomLeft,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                  "More Info",
-                  style: TextStyle(fontSize: 20.0),
+                RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                  color: Colors.purple[400],
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/4.jpg',
+                        height: 140.0,
+                        width: 140.0,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-          ),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: FlatButton(
-                color: Colors.purple[400],
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                onPressed: () {},
-                child: Text(
-                  "Reminder",
-                  style: TextStyle(fontSize: 20.0),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                  color: Colors.purple[400],
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Image.asset(
+                        'images/6.jpg',
+                        height: 140.0,
+                        width: 140.0,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       )
     );
   }
--- lib/settings.dart
+++ lib/settings.dart
@@ -5,12 +5,80 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(child: Text("Profile Page",style: TextStyle(fontSize: 20),))
-        ],
-      ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                    color: Colors.purple[400],
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Image.asset(
+                          'images/1.jpg',
+                          height: 140.0,
+                          width: 140.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                  RaisedButton(
+                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                    color: Colors.purple[400],
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Image.asset(
+                          'images/2.jpg',
+                          height: 140.0,
+                          width: 140.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+                    color: Colors.purple[400],
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.black,
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {},
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Image.asset(
+                          'images/3.jpg',
+                          height: 140.0,
+                          width: 140.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
     );
   }
 }
